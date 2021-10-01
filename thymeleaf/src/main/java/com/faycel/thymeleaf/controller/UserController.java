f187d7d5d9bbc0e0241c4835cf9bcab7f2c13c65
Faycel Bouslahi <faicel.bouslahi@gmail.com>
2021-10-01 15:04:00 +0100

    Thymelaf Demo
    Thymelaf - Variable + Condition + Loop

--- thymeleaf/src/main/java/com/faycel/thymeleaf/controller/UserController.java
+++ thymeleaf/src/main/java/com/faycel/thymeleaf/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.faycel.thymeleaf.controller;
 
+import com.faycel.thymeleaf.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -16,6 +20,18 @@ public class UserController {
         model.addAttribute("GPA", convertGPA(grade));
         // return to template/demo.html page.
         return"demo";
+    }
+
+    // Spring Boot Template Engine - Thymeleaf - Loop
+
+    @RequestMapping("demo2")
+    public String demo2(Model model) {
+        List<User> lst = new ArrayList<>();
+        lst.add(new User(1, "Ali", 30));
+        lst.add(new User(2, "Mohamed", 30));
+        lst.add(new User(3, "Adam", 27));
+        model.addAttribute("list", lst);
+        return "demo2";
     }
 
     private String convertGPA(double grade) {
