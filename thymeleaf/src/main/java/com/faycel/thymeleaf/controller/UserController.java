287e01f57423826972394236f6efbd7a978c34f2
Faycel Bouslahi <faicel.bouslahi@gmail.com>
2021-10-01 15:26:14 +0100

    Thymelaf Demo
    Thymelaf - Variable + Condition + Loop + URL

--- thymeleaf/src/main/java/com/faycel/thymeleaf/controller/UserController.java
+++ thymeleaf/src/main/java/com/faycel/thymeleaf/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,6 +34,21 @@ public class UserController {
         model.addAttribute("list", lst);
         return "demo2";
     }
+
+    // Spring Boot Template Engine - Thymeleaf - Scoop
+
+    @RequestMapping("demo3")
+    public String demo3(HttpServletRequest request, Model model) {
+        // Request
+        request.setAttribute("request", "request data");
+        // Session
+        request.getSession().setAttribute("session", "session data");
+        // Application
+        request.getSession().getServletContext().setAttribute("application", "application data");
+        return "demo2";
+    }
+
+    // Spring Boot Template Engine - Thymeleaf - Condition
 
     private String convertGPA(double grade) {
         if (grade >= 19) {
