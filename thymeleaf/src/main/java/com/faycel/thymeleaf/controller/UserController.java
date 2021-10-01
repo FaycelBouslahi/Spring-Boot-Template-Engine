e46a3754c07dc8c163a5028ca37bfccc73fa6c15
Faycel Bouslahi <faicel.bouslahi@gmail.com>
2021-10-01 14:11:57 +0100

    Hello Thymeleaf !
    Thymleaf first program ;)

--- thymeleaf/src/main/java/com/faycel/thymeleaf/controller/UserController.java
+++ thymeleaf/src/main/java/com/faycel/thymeleaf/controller/UserController.java
@@ -1,4 +1,16 @@
 package com.faycel.thymeleaf.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class UserController {
+
+    @RequestMapping("demo")
+    public String demo(Model model) {
+        model.addAttribute("message", "Hello Thymeleaf !");
+        // return to template/demo.html page.
+        return"demo";
+    }
 }
