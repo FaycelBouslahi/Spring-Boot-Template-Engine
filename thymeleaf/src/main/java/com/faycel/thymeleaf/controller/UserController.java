c6cf4f6d92b040f485412204710a76ce0f2894dc
Faycel Bouslahi <faicel.bouslahi@gmail.com>
2021-10-01 14:33:34 +0100

    Thymelaf - Variable + Condition
    Thymeleaf - Condition

--- thymeleaf/src/main/java/com/faycel/thymeleaf/controller/UserController.java
+++ thymeleaf/src/main/java/com/faycel/thymeleaf/controller/UserController.java
@@ -10,7 +10,28 @@ public class UserController {
     @RequestMapping("demo")
     public String demo(Model model) {
         model.addAttribute("message", "Hello Thymeleaf !");
+        // First variable with Thymeleaf
+        double grade = 18;
+        model.addAttribute("grade", grade);
+        model.addAttribute("GPA", convertGPA(grade));
         // return to template/demo.html page.
         return"demo";
+    }
+
+    private String convertGPA(double grade) {
+        if (grade >= 19) {
+            return  "A";
+        } else if (grade < 19 && grade >= 15 ) {
+            return  "B";
+        } else if (grade < 15 && grade >= 10 ) {
+            return  "C";
+        } else if (grade < 10 && grade >= 3 ) {
+            return  "D";
+        } else {
+            return ("We Will Fix This Together !");
+        }
+
+
+
     }
 }
