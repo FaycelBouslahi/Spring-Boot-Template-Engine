605b4147a858b7c42b5b27972a1f289d2b03ca78
Faycel Bouslahi <faicel.bouslahi@gmail.com>
2021-10-08 12:48:03 +0100

    Spring Boot Unit Test
    Unit Test Demo

---
+++ unit test/src/main/java/com/faycel/springbootunittest/service/UserService.java
@@ -0,0 +1,16 @@
+package com.faycel.springbootunittest.service;
+
+import com.faycel.springbootunittest.dao.UserDao;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class UserService {
+    @Resource
+    private UserDao userDao;
+
+    public String getUser() {
+        return userDao.getUser();
+    }
+}
---
+++ unit test/src/test/java/com/faycel/springbootunittest/SpringBootUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faycel.springbootunittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUnitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
